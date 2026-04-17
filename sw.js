cf834101548d8b3cf0d9d43b627110e7b4feffff
janithdidula025-design <janithdidula025@gmail.com>
2026-04-17 23:18:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,29 @@
-const CACHE_NAME = 'nexcall-v1';
+const CACHE_NAME = 'nexcall-cache-v' + Date.now();
 const ASSETS = [
+  '/',
   'index.html',
   'manifest.json',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => res || fetch(event.request))
+    );
 });
